43a57075ce3e32b20b98a70928a8a4af1b24eaa3
Cassidy Scheffer <cscheffer@wealthsimple.com>
2021-12-30 16:30:18 -0500

    prettier

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { mapSchema, getDirective, MapperKind, pruneSchema } from '@graphql-tools/utils'
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { Transform } from '@graphql-tools/delegate';
+import { ApolloServer, gql } from "apollo-server";
+import {
+  mapSchema,
+  getDirective,
+  MapperKind,
+  pruneSchema,
+} from "@graphql-tools/utils";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { Transform } from "@graphql-tools/delegate";
 import {
   wrapSchema,
   FilterRootFields,
   FilterObjectFields,
   FilterTypes,
-  FilterInterfaceFields
-} from '@graphql-tools/wrap';
+  FilterInterfaceFields,
+} from "@graphql-tools/wrap";
 
 const typeDefs = gql`
-  directive @internal on
-    | OBJECT
-    | FIELD_DEFINITION
+  directive @internal on OBJECT | FIELD_DEFINITION
 
   type Client {
     fullName: String
@@ -30,21 +33,16 @@ const typeDefs = gql`
   }
 `;
 
-const clients: Array<Object>= [
-  {
-    fullName: 'Merlin Counting Stars',
-    tin: '12345',
-  },
-  { fullName: 'Rythem n Blues',
-    tin: '9876',
-  }
+const clients: Array<Object> = [
+  { fullName: "Merlin Counting Stars", tin: "12345" },
+  { fullName: "Rythem n Blues", tin: "9876" },
 ];
 
 const secrets = [
   {
-    shhh: "nothing to see here"
-  }
-]
+    shhh: "nothing to see here",
+  },
+];
 
 const resolvers = {
   Query: {
@@ -53,48 +51,46 @@ const resolvers = {
   },
 };
 
-
 const isPublic: boolean = true;
 
 const transforms: Array<Transform> = [
   new FilterRootFields((_operationName, _fieldName, fieldConfig) => {
-    const isInternal = getDirective(schema, fieldConfig, 'internal')?.[0];
+    const isInternal = getDirective(schema, fieldConfig, "internal")?.[0];
     if (isPublic) {
-      return !isInternal
+      return !isInternal;
     } else {
-      return true
-    };
+      return true;
+    }
   }),
 
   new FilterObjectFields((_operationName, _fieldName, fieldConfig) => {
-    const isInternal = getDirective(schema, fieldConfig, 'internal')?.[0];
+    const isInternal = getDirective(schema, fieldConfig, "internal")?.[0];
     if (isPublic) {
-      return !isInternal
+      return !isInternal;
     } else {
-      return true
-    };
+      return true;
+    }
   }),
 
   new FilterTypes((graphQLNamedType) => {
-    const isInternal = getDirective(schema, graphQLNamedType, 'internal')?.[0];
+    const isInternal = getDirective(schema, graphQLNamedType, "internal")?.[0];
     if (isPublic) {
-      return !isInternal
+      return !isInternal;
     } else {
-      return true
-    };
+      return true;
+    }
   }),
-
 ];
 
 let schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-})
+});
 
-schema = wrapSchema({ schema, transforms })
+schema = wrapSchema({ schema, transforms });
 
 const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
-  console.log(`😎 Server ready at ${ url }`);
+  console.log(`😎 Server ready at ${url}`);
 });
